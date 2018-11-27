9105be5682a9e92d8a46f7a8c23db5a93080e88c
Troy Neubauer <troyneubauer@gmail.com>
2018-11-26 23:07:09 -0500

    Test Rickshaw graph!

--- Code/Node Server/Server.js
+++ Code/Node Server/Server.js
@@ -6,60 +6,73 @@ var http = require("http"),
     fs = require("fs")
     port = 8192;
 
-	server = http.createServer(function(request, response) {/*
-		console.log("\t\t\treq headers: " + request.headers);
-		console.log("\t\t\treq httpVersion: " + request.httpVersion);
-		console.log("\t\t\treq method: " + request.method);
-		console.log("\t\t\treq rawHeaders: " + request.rawHeaders);
-		console.log("\t\t\treq rawTrailers: " + request.rawTrailers);
-		console.log("\t\t\treq trailers: " + request.trailers);*/
-		var uri = url.parse(request.url).pathname, filename = path.join("C:/Users/Troy Neubauer/Documents/Rocketry/Code/Website", uri);
+server = http.createServer(function(request, response) {
+	var contentTypesByExtension = {
+		'.html': "text/html",
+		'.css':  "text/css",
+		'.js':   "text/javascript"
+	};
+	var uri = url.parse(request.url).pathname;
+	var filename1 = path.join("C:/Users/Troy Neubauer/Documents/Rocketry/Code/Website", uri);
+	var filename2 = path.join("C:/Users/Troy Neubauer/AppData/Roaming/npm/node_modules", uri);
+
+	if(fs.existsSync(filename1)) {
+		console.log("#### found " + uri);
 
 
+		if (fs.statSync(filename1).isDirectory()) filename1 += '/index.html';
 
-		var contentTypesByExtension = {
-			'.html': "text/html",
-			'.css':  "text/css",
-			'.js':   "text/javascript"
-		};
+		fs.readFile(filename1, "binary", function(err, file) {
+			if(err) {
+				response.writeHead(500, {"Content-Type": "text/plain"});
+				response.write(err + "\n");
+				response.end();
+				return;
+			}
 
-		fs.exists(filename, function(exists) {
-		if(!exists) {
-			response.writeHead(404, {"Content-Type": "text/plain"});
-			response.write("404 Not Found\n");
-			console.log("Coundnt file file: " + uri + " at " + filename);
-			response.end();
-			return;
-		} else {
-			console.log("#### found" + uri);
-		}
-
-		if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-		fs.readFile(filename, "binary", function(err, file) {
-		if(err) {
-			response.writeHead(500, {"Content-Type": "text/plain"});
-			response.write(err + "\n");
-			response.end();
-			return;
-		}
-
-		var headers = {};
-		var contentType = contentTypesByExtension[path.extname(filename)];
-		if (contentType) headers["Content-Type"] = contentType;
+			var headers = {};
+			var contentType = contentTypesByExtension[path.extname(filename1)];
+			if (contentType) headers["Content-Type"] = contentType;
 			response.writeHead(200, headers);
 			response.write(file, "binary");
 			response.end();
 		});
-	});
+	} else if(fs.existsSync(filename2)) {
+		console.log(".... found in location 2 " + uri);
+
+		if (fs.statSync(filename2).isDirectory()) filename2 += '/index.html';
+
+		fs.readFile(filename2, "binary", function(err, file) {
+			if(err) {
+				response.writeHead(500, {"Content-Type": "text/plain"});
+				response.write(err + "\n");
+				response.end();
+				return;
+			}
+
+			var headers = {};
+			var contentType = contentTypesByExtension[path.extname(filename2)];
+			if (contentType) headers["Content-Type"] = contentType;
+			response.writeHead(200, headers);
+			response.write(file, "binary");
+			response.end();
+		});
+	} else {
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.write("404 Not Found\n");
+		console.log("Coundnt file file: " + uri + " at " + filename2);
+		response.end();
+		return;
+	}
 });
+
 server.listen(port);
 
 var io = require('socket.io').listen(server);
 
 io.on('connection', function(socket) {
     io.emit('Server 2 Client Message', 'Welcome!' );
-    socket.on('Client 2 Server Message', function(message)      {
+    socket.on('Client 2 Server Message', function(message) {
         console.log(message);
         io.emit('Server 2 Client Message', message.toUpperCase());     //upcase it
     });
@@ -88,7 +101,6 @@ var rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
-
 
 
 var bytes = 0;
