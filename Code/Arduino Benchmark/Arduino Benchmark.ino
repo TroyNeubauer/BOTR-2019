92b76e2d44e27c11d83cc331dc5b34a43dfde035
Troy Neubauer <troyneubauer@gmail.com>
2018-12-26 10:58:14 -0700

    Benchmarks complete

--- Code/Arduino Benchmark/Arduino Benchmark.ino
+++ Code/Arduino Benchmark/Arduino Benchmark.ino
@@ -1,25 +1,78 @@
 #include <stdint.h>
 #include <Arduino.h>
+#include <math.h>
+
+//On arduino mega 2560
+//int	+: 1.45 micros
+//		-: 1.51 micros
+//		*: 1.96 micros
+//		/: 15.63 micros
+
+//float	+: 20.99 micros
+//		-: 21.87 micros
+//		*: 22.42 micros
+//		/: 44.29 micros
+
+//double+: 20.81 micros
+//		-: 21.69 micros
+//		*: 22.12 micros
+//		/: 43.97 micros
+//	atan(f) / sqrt(f2): 267.46 micros
 
 #define TEST_COUNT 5000
-#define TYPE int
-#define OPERATION f + f
+#define TYPE float
+#define OPERATION atanf(f) / sqrtf(f2)
+//#define OPERATION f / f2
 
-//int	+: 0.69 micros
-//		-: 0.5 micros
-//		*:
-//		/:
+//On arduino due
+//int	+: 0.27 micros
+//		-: 0.27 micros
+//		*: 0.25 micros
+//		/: 0.25 micros
 
+//float	+: 0.33 micros
+//		-: 0.33 micros
+//		*: 2.76 micros - 10X slower than int *
+//		/: 4.33 micros - 20X slower than int /
+//	atanf(f) / sqrtf(f2): 36.28 micros
+
+//double+: 4.35 micros
+//		-: 4.47 micros
+//		*: 4.00 micros
+//		/: 13.31 micros
+//	atan(f) / sqrt(f2): 85.38 micros
+#define STRING2(X) #X
+#define STRING(X) STRING2(X)
 
 void setup() {
 	Serial.begin(115200);
+	while (!Serial)//Wait for the serialport to connect
+		;
+	TYPE items[TEST_COUNT];//The array that will store the results
+	//We have to use an array so that the compiler doesnt optomize away the operation we are testing
+	TYPE f, f2;
 
-	TYPE items[TEST_COUNT];
+	Serial.println();
+	Serial.println();
+	Serial.println();
+	Serial.println();
+	Serial.println();
+	Serial.println();
+	Serial.println();
+	Serial.println();
+	Serial.println("Starting test...");
+	Serial.print("Type ");
+	Serial.print(STRING(TYPE));
+	Serial.print("Size: ");
+	Serial.println(sizeof(TYPE));
 
-	TYPE f;
+	Serial.print("Array size ");
+	Serial.println(sizeof(items));
+
 	uint32_t start = micros();
 	for (int i = 0; i < TEST_COUNT; i++) {
 		f = (TYPE)i;
+		f2 = (TYPE)i * f;
 		TYPE result = OPERATION;
 		items[i] = result;
 	}
@@ -28,8 +81,12 @@ void setup() {
 	Serial.print(F("Total time in micros: "));
 	Serial.println(time);
 	Serial.print(F("Average time per iteration: "));
-	Serial.println((float)time / (float)TEST_COUNT);
-	Serial.println();
+	float averageTime = (float)time / (float)TEST_COUNT;
+	Serial.println(averageTime);
+
+	Serial.print(F("Number of operations per microsecond "));
+	Serial.println(1.0f / averageTime);
+
 	for (int i = 0; i < TEST_COUNT; i++) {
 		Serial1.print(items[i]);
 		Serial1.print(',');
---
+++ Code/Arduino Benchmark/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\Troy\ Neubauer\AppData\Local\Temp\VMBuilds\Arduino\ Benchmark\mega_atmega2560\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Community\Common7\IDE\Extensions\ktpce3c3.a4z\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
